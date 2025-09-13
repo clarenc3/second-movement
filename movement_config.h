f8340f1c96b9d1df9b3b606c9c8e993a83049cc6
Clarence <c.wret@rochester.edu>
2025-09-13 16:45:20 +0100

    Add preferred faces

--- movement_config.h
+++ movement_config.h
@@ -29,6 +29,24 @@
 
 const watch_face_t watch_faces[] = {
     clock_face,
+    sunrise_sunset_face,
+    stopwatch_face,
+    countdown_face,
+    advanced_alarm_face,
+    moon_phase_face,
+    world_clock_face,
+    accelerometer_status_face,
+    activity_logging_face,
+    temperature_display_face,
+    temperature_logging_face,
+    voltage_face,
+    settings_face,
+    set_time_face
+};
+
+/*
+const watch_face_t watch_faces[] = {
+    clock_face,
     world_clock_face,
     sunrise_sunset_face,
     moon_phase_face,
@@ -40,6 +58,7 @@ const watch_face_t watch_faces[] = {
     settings_face,
     set_time_face
 };
+*/
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
 
@@ -49,10 +68,11 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 7)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
-#define SIGNAL_TUNE_DEFAULT
+//#define SIGNAL_TUNE_DEFAULT
+#define SIGNAL_TUNE_MARIO_THEME
 
 /* Determines the intensity of the led colors
  * Set a hex value 0-15 with 0x0 being off and 0xF being max intensity
@@ -62,7 +82,7 @@ const watch_face_t watch_faces[] = {
 #define MOVEMENT_DEFAULT_BLUE_COLOR 0x0
 
 /* Set to true for 24h mode or false for 12h mode */
-#define MOVEMENT_DEFAULT_24H_MODE false
+#define MOVEMENT_DEFAULT_24H_MODE true
 
 /* Enable or disable the sound on mode button press */
 #define MOVEMENT_DEFAULT_BUTTON_SOUND true
